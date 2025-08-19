315e3895ce95e593ffc37ef8b331ad5218e7b546
dbingabire05@gmail.com <dbingabire05@gmail.com>
2025-08-19 16:06:30 +0200

    edited th routes

--- vanlife_project/src/App.jsx
+++ vanlife_project/src/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import { BrowserRouter, Routes, Route,Link } from 'react-router-dom'
 import './index.css'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+   <BrowserRouter>
+   <header className="">
+    <Link to="/">#VANLIFE</Link>
+    <nav>
+        <Link to="/about">About</Link>
+    </nav>
+   </header>
+   <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/about" element={<About />} />
+   {/* Catch-all route */}
+        <Route path="*" element={<NotFound />} />
+   </Routes>
+   </BrowserRouter>
+
   )
 }
 
---
+++ vanlife_project/src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+const NotFound = () => {
+  return (
+    <div className="notfound-container">
+      <h1>404</h1>
+      <p>Oops! Page not found.</p>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
